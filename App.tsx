9bffceed6cb605d32a35f4d8df20c10bab8e013e
Pavel <>
2024-03-03 23:44:11 +0200

    fix: fixed warnings

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { TamaguiProvider, View, createTamagui, useTheme } from '@tamagui/core';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Navigation } from './src/navigation';
 import { TamaguiThemeProvider } from './src/theme';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreAllLogs(true);
 
 function App(): React.JSX.Element {
   return (
